a401c3ece68d2d3951c59db4f2f1aafd1ce035d0
Thomas Harrison <twh2898@vt.edu>
2017-12-22 11:17:22 -0500

    Added chrono, implemented library

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,36 @@
+
+extern crate chrono;
+use chrono::{DateTime, Utc};
+
+pub struct TSData<D> {
+    data: D,
+    timestamp: DateTime<Utc>,
+}
+
+impl<D> TSData<D> {
+    pub fn new(data: D) -> Self {
+        TSData {
+            data: data,
+            timestamp: Utc::now(),
+        }
+    }
+
+    pub fn with_timestamp(self, timestamp: DateTime<Utc>) -> Self {
+        TSData {
+            timestamp: timestamp,
+            ..self
+        }
+    }
+
+    pub fn data(&self) -> &D {
+        &self.data
+    }
+
+    pub fn timestamp(&self) -> DateTime<Utc> {
+        self.timestamp.clone()
+    }
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
